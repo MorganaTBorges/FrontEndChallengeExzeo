6c03b78c23327d816a43d6c4acc09fada1a72e5b
Morgana Borges <morgana.t.borges@gmail.com>
2020-09-23 19:02:00 -0300

    parsin json

---
+++ app/controllers/concerns/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  require 'json'
+  require 'open-uri'
+  
+    def home
+      url = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json'
+      file_serialized = open(url).read
+      file = JSON.parse(file_serialized)
+      @albums = file["feed"]["entry"]
+      # @artist = @albums["im:artist"]["label"]
+    end
+end
+  
